28b5dcd73f3928ad8814b7a1d7863ad492588f26
ExplodingAlt <74530583+ExplodingAlt@users.noreply.github.com>
2020-12-21 01:54:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
-
-<!DOCTYPE html>
-<html>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Test</title>
-<head>
-  <font color="white">
-    <h1 style="text-align:center; position: absolute; top: 1%; left: 7%"; >test</h1>
-      <meta name="description" content="testtt" />
-<style>
-</style>
-</head>
-<body style="background-color: #000000">
-      <img style="display: none;" src="updater.php">
-</body>
-</html>
+<? php
+echo '';
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<title>Test</title>';
+echo '<head>';
+echo '<font color="white">';
+echo '<h1 style="text-align:center; position: absolute; top: 1%; left: 7%"; >test</h1>';
+echo '<meta name="description" content="testtt" />';
+echo '<style>';
+echo '</style>';
+echo '</head>';
+echo '<body style="background-color: #000000">';
+echo '<img style="display: none;" src="updater.php">';
+echo '</body>';
+echo '</html>';
+echo '';
+?>
